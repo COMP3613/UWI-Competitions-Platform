21d7a2ae3bba028849d7fe3cdc90daf9b8fd2e7a
Trinity <Joshcabral@hotmail.com>
2024-11-22 20:24:16 -0400

    Refactor Student model/controller for historical ranking

--- App/models/student.py
+++ App/models/student.py
@@ -1,8 +1,8 @@
 from App.database import db
 from App.models import User
 from App.models.observer import Observer
-from App.models.ranking_caretaker import RankingCaretaker
-from App.models.ranking_originator import RankingOriginator
+from App.models.student_memento import StudentMemento
+
 
 class Student(User, Observer):
     __tablename__ = 'student'
@@ -15,9 +15,6 @@ class Student(User, Observer):
     notifications = db.relationship('Notification', backref='student', lazy=True)
     competitions = db.relationship('Competition', secondary='competition_observer', back_populates='observers')
 
-    originator = RankingOriginator()
-    caretaker = RankingCaretaker()
-
     def __init__(self, username, password):
         super().__init__(username, password)
         self.rating_score = 0
@@ -29,22 +26,33 @@ class Student(User, Observer):
 
     def add_notification(self, notification):
         if notification:
-          try:
-            self.notifications.append(notification)
-            db.session.commit()
-            return notification
-          except Exception as e:
-            db.session.rollback()
-            return None
+            try:
+                self.notifications.append(notification)
+                db.session.commit()
+                return notification
+            except Exception as e:
+                db.session.rollback()
+                return None
         return None
+
+    def create_memento(self):
+        memento = StudentMemento(
+            student_id=self.id,
+            username = self.username,
+            rating_score=self.rating_score,
+            curr_rank=self.curr_rank,
+        )
+        db.session.add(memento)
+        db.session.commit()
+        return memento
 
     def get_json(self):
         return {
             "id": self.id,
             "username": self.username,
             "rating_score": self.rating_score,
-            "comp_count" : self.comp_count,
-            "curr_rank" : self.curr_rank
+            "comp_count": self.comp_count,
+            "curr_rank": self.curr_rank
         }
 
     def to_Dict(self):
@@ -52,19 +60,20 @@ class Student(User, Observer):
             "ID": self.id,
             "Username": self.username,
             "Rating Score": self.rating_score,
-            "Number of Competitions" : self.comp_count,
-            "Rank" : self.curr_rank
+            "Number of Competitions": self.comp_count,
+            "Rank": self.curr_rank
         }
 
     def __repr__(self):
         return f'<Student {self.id} : {self.username}>'
 
-    def update_rankings(self):
-       self.caretaker.save_memento(self.originator.create_memento())
-       print(f"Updating rankings for {self.username}...")
+    # def update_rankings(self):
+    #     self.caretaker.save_memento(self.originator.create_memento())
+    #     print(f"Updating rankings for {self.username}...")
+
 
 # Define the association table
 competition_observer = db.Table('competition_observer',
-    db.Column('competition_id', db.Integer, db.ForeignKey('competition.id')),
-    db.Column('student_id', db.Integer, db.ForeignKey('student.id'))
-)
+                                db.Column('competition_id', db.Integer, db.ForeignKey('competition.id')),
+                                db.Column('student_id', db.Integer, db.ForeignKey('student.id'))
+                                )
