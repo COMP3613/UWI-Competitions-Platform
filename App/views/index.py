482149f5c522143cb42611dca71420099f3bfee2
Trinity <Joshcabral@hotmail.com>
2024-12-03 19:12:11 -0400

    Update designs for most page UIs

--- App/views/index.py
+++ App/views/index.py
@@ -10,13 +10,17 @@ invoker = CommandInvoker()
 
 index_views = Blueprint('index_views', __name__, template_folder='../templates')
 
+
 @index_views.route('/', methods=['GET'])
 def home_page():
     return render_template('leaderboard.html', leaderboard=display_rankings(), user=current_user)
 
+
 @index_views.route('/leaderboard', methods=['GET'])
 def leaderboard_page():
-    return render_template('leaderboard.html', leaderboard=display_rankings(), user=current_user)#, competitions=get_all_competitions(), moderators=get_all_moderators())
+    return render_template('leaderboard.html', leaderboard=display_rankings(),
+                           user=current_user)  # , competitions=get_all_competitions(), moderators=get_all_moderators())
+
 
 @index_views.route('/init', methods=['GET'])
 def init():
@@ -100,52 +104,55 @@ def init():
     update_rankings()
     """
 
-    #creates students
+    # creates students
     with open("students.csv") as student_file:
         reader = csv.DictReader(student_file)
 
         for student in reader:
             stud = create_student(student['username'], student['password'])
-            #db.session.add(stud)
-        #db.session.commit()
-    
+            # db.session.add(stud)
+        # db.session.commit()
+
     student_file.close()
 
-    #creates moderators
+    # creates moderators
     with open("moderators.csv") as moderator_file:
         reader = csv.DictReader(moderator_file)
 
         for moderator in reader:
             mod = create_moderator(moderator['username'], moderator['password'])
-            #db.session.add(mod)
-        #db.session.commit()
-    
+            # db.session.add(mod)
+        # db.session.commit()
+
     moderator_file.close()
 
-    #creates competitions
+    # creates competitions
     with open("competitions.csv") as competition_file:
         invoker = CommandInvoker()
         reader = csv.DictReader(competition_file)
 
         for competition in reader:
-            invoker.set_on_start(AddCompetition(competition['mod_name'], competition['comp_name'], competition['date'], competition['location'], competition['level'], competition['max_score']))
+            invoker.set_on_start(AddCompetition(competition['mod_name'], competition['comp_name'], competition['date'],
+                                                competition['location'], competition['level'],
+                                                competition['max_score']))
             invoker.execute_command()
-    
+
     competition_file.close()
-    
+
     with open("results.csv") as results_file:
         reader = csv.DictReader(results_file)
 
         for result in reader:
             students = [result['student1'], result['student2'], result['student3']]
-            #team = add_team(result['mod_name'], result['comp_name'], result['team_name'], students)
-            #add_results(result['mod_name'], result['comp_name'], result['team_name'], int(result['score']))
-            invoker.set_on_start(AddResults(result['mod_name'], result['comp_name'],  result['team_name'], students, int(result['score'])))
+            # team = add_team(result['mod_name'], result['comp_name'], result['team_name'], students)
+            # add_results(result['mod_name'], result['comp_name'], result['team_name'], int(result['score']))
+            invoker.set_on_start(AddResults(result['mod_name'], result['comp_name'], result['team_name'], students,
+                                            int(result['score'])))
             invoker.execute_command()
 
-            #db.session.add(comp)
-        #db.session.commit()
-    
+            # db.session.add(comp)
+        # db.session.commit()
+
     results_file.close()
 
     with open("competitions.csv") as competitions_file:
@@ -153,15 +160,15 @@ def init():
 
         for competition in reader:
             if competition['comp_name'] != 'TopCoder':
-                #update_ratings(competition['mod_name'], competition['comp_name'])
-                #update_rankings()
+                # update_ratings(competition['mod_name'], competition['comp_name'])
+                # update_rankings()
                 invoker.set_on_start(UpdateRating(competition))
                 invoker.set_on_finish(UpdateRank())
                 invoker.execute_command()
 
-            #db.session.add(comp)
-        #db.session.commit()
-    
+            # db.session.add(comp)
+        # db.session.commit()
+
     competitions_file.close()
 
     return render_template('leaderboard.html', leaderboard=display_rankings(), user=current_user)
@@ -179,11 +186,12 @@ def health():
  #   return render_template('Student_Profile.html', user_id=user_id)
 """
 
+
 @index_views.route('/profile')
 def profile():
     user_type = session['user_type']
     id = current_user.get_id()
-    
+
     if user_type == 'moderator':
         template = moderator_profile(id)
 
@@ -192,11 +200,13 @@ def profile():
 
     return template
 
+
 @index_views.route('/student_profile/<int:id>', methods=['GET'])
 def student_profile(id):
     student = get_student(id)
     if not student:
-        return render_template('student_profile.html', student=None, competitions=None, user=current_user,historical_data=None, id = id)
+        return render_template('student_profile.html', student=None, competitions=None, user=current_user,
+                               historical_data=None, id=id)
 
     history = get_historical_ranking(student)
     profile_info = display_student_info(student.username)
@@ -208,7 +218,9 @@ def student_profile(id):
     notifications= get_notifications(user.username)
     """
 
-    return render_template('student_profile.html', student=student, competitions=competitions, user=current_user,historical_data=history)
+    return render_template('student_profile.html', student=student, competitions=competitions, user=current_user,
+                           historical_data=history)
+
 
 @index_views.route('/student_profile/<string:name>', methods=['GET'])
 def student_profile_by_name(name):
@@ -216,9 +228,18 @@ def student_profile_by_name(name):
 
     if not student:
         return render_template('404.html')
-    
+
     profile_info = display_student_info(student.username)
-    competitions = profile_info['competitions']
+    competitionsQuery = profile_info['competitions']
+    competitions = []
+    for competition in competitionsQuery:
+        comp = get_competition_by_name(competition)
+        print(comp)
+        competitions.append({
+            "name": comp.name,
+            "date": comp.date,
+            "id": comp.id
+        })
     history = get_historical_ranking(student)
     """
     competitions = Competition.query.filter(Competition.participants.any(id=user_id)).all()
@@ -226,10 +247,12 @@ def student_profile_by_name(name):
     notifications= get_notifications(user.username)
     """
 
-    return render_template('student_profile.html', student=student, competitions=competitions, user=current_user, history=history)
+    return render_template('student_profile.html', student=student, competitions=competitions, user=current_user,
+                           history=history)
+
 
 @index_views.route('/moderator_profile/<int:id>', methods=['GET'])
-def moderator_profile(id):   
+def moderator_profile(id):
     moderator = get_moderator(id)
 
     if not moderator:
@@ -279,7 +302,9 @@ def create_moderator():
         return jsonify({'message': f"Moderator: {mod.username} created!"})
     else:
         return jsonify({'message': "Failed to create moderator!"})
-"""       
+"""
+
+
 """
 @index_views.route('/login')
 def login():
@@ -294,46 +319,47 @@ def signup():
     return render_template('signup.html')
 """
 
+
 @index_views.route('/init_postman', methods=['GET'])
 def init_postman():
-    
     db.drop_all()
     db.create_all()
-    
 
-    #creates students
+    # creates students
     with open("students.csv") as student_file:
         reader = csv.DictReader(student_file)
 
         for student in reader:
             stud = create_student(student['username'], student['password'])
-            #db.session.add(stud)
-        #db.session.commit()
-    
+            # db.session.add(stud)
+        # db.session.commit()
+
     student_file.close()
 
-    #creates moderators
+    # creates moderators
     with open("moderators.csv") as moderator_file:
         reader = csv.DictReader(moderator_file)
 
         for moderator in reader:
             mod = create_moderator(moderator['username'], moderator['password'])
-            #db.session.add(mod)
-        #db.session.commit()
-    
+            # db.session.add(mod)
+        # db.session.commit()
+
     moderator_file.close()
 
-    #creates competitions
+    # creates competitions
     with open("competitions.csv") as competition_file:
         invoker = CommandInvoker()
         reader = csv.DictReader(competition_file)
 
         for competition in reader:
-            invoker.set_on_start(AddCompetition(competition['mod_name'], competition['comp_name'], competition['date'], competition['location'], competition['level'], competition['max_score']))
+            invoker.set_on_start(AddCompetition(competition['mod_name'], competition['comp_name'], competition['date'],
+                                                competition['location'], competition['level'],
+                                                competition['max_score']))
             invoker.execute_command()
-    
+
     competition_file.close()
-    
+
     with open("results.csv") as results_file:
         reader = csv.DictReader(results_file)
 
@@ -341,9 +367,9 @@ def init_postman():
             students = [result['student1'], result['student2'], result['student3']]
             team = add_team(result['mod_name'], result['comp_name'], result['team_name'], students)
             add_results(result['mod_name'], result['comp_name'], result['team_name'], int(result['score']))
-            #db.session.add(comp)
-        #db.session.commit()
-    
+            # db.session.add(comp)
+        # db.session.commit()
+
     results_file.close()
 
     with open("competitions.csv") as competitions_file:
@@ -352,9 +378,9 @@ def init_postman():
         for competition in reader:
             update_ratings(competition['mod_name'], competition['comp_name'])
             update_rankings()
-            #db.session.add(comp)
-        #db.session.commit()
-    
+            # db.session.add(comp)
+        # db.session.commit()
+
     competitions_file.close()
 
-    return (jsonify({'message': "database_initialized"}),200)
+    return (jsonify({'message': "database_initialized"}), 200)
